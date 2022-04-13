5cc82574afb117e4cf08700823a80e6bbb7840b0
itsajay2195 <ajayqae95@gmail.com>
2022-04-13 13:43:40 +0530

    added navigate to debit button to the coming soon component

--- src/components/common/ComingSoon.js
+++ src/components/common/ComingSoon.js
@@ -3,8 +3,13 @@ import { COLORS } from '../../constants'
 import React from 'react'
 import LottieView from 'lottie-react-native'
 import { useNavigation } from '@react-navigation/native'
+import PrimaryButton from './PrimaryButton'
+
 const ComingSoon = () => {
     const navigation = useNavigation()
+    const onBtnClick=()=>{
+      navigation.navigate("Debit Card")
+    }
     return (
         <View style={{ flex: 1,justifyContent:'center',alignItems:'center',backgroundColor:COLORS.white }}>
            <LottieView
@@ -18,10 +23,11 @@ const ComingSoon = () => {
           source={require('../../assets/lottie/coming-soon.json')}
           // OR find more Lottie files @ https://lottiefiles.com/featured
           // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
-        />
-           <TouchableOpacity onPress={()=>navigation.navigate('Debit Card')}>
-               <Text>Go to Debit Card</Text>
-           </TouchableOpacity>
+        />  
+          <View style={{width:'100%',paddingVertical:20}} >
+             <PrimaryButton btnText={"Go to Debit Card"} onPress={onBtnClick}/>
+          </View>
+            
         </View>
     );
 }
